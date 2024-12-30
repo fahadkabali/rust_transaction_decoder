a1fbed16067677ae88fbb522ab42a873a131f06b
fahadkabali <kabalifahad@gmail.com>
2024-12-30 04:25:27 +0300

    removed target files

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,43 @@
 use std::io::Read;
+fn read_compact_size(transaction_bytes: &mut &[u8]) -> u64 {
+    let mut compact_size = [0_u8; 1];
+    transaction_bytes.read(&mut compact_size).unwrap();
+
+    match compact_size[0] {
+        0..=252 => compact_size[0] as u64,
+        253 => {
+            let mut buffer = [0; 2];
+            transaction_bytes.read(&mut buffer).unwrap();
+            u16::from_le_bytes(buffer) as u64
+        }
+        254 => {
+            let mut buffer = [0; 4];
+            transaction_bytes.read(&mut buffer).unwrap();
+            u32::from_le_bytes(buffer) as u64
+        }
+        255 => {
+            let mut buffer = [0; 8];
+            transaction_bytes.read(&mut buffer).unwrap();
+            u64::from_le_bytes(buffer)
+        }
+    }
+    // if compact_size[0] < 0xfd {
+    //     return compact_size[0] as u64;
+    // }
+    // if compact_size[0] == 0xfd {
+    //     let mut buffer = [0_u8; 2];
+    //     transaction_bytes.read(&mut buffer).unwrap();
+    //     u16::from_le_bytes(buffer) as u64
+    // } else if compact_size[0] == 0xfe {
+    //     let mut buffer = [0_u8; 4];
+    //     transaction_bytes.read(&mut buffer).unwrap();
+    //     u32::from_le_bytes(buffer) as u64
+    // } else {
+    //     let mut buffer = [0_u8; 8];
+    //     transaction_bytes.read(&mut buffer).unwrap();
+    //     u64::from_le_bytes(buffer)
+    // }
+}
 fn read_version(transaction_bytes: &mut &[u8]) -> u32 {
     let mut buffer = [0; 4];
     transaction_bytes.read(&mut buffer).unwrap();
@@ -17,6 +56,8 @@ fn main() {
     let transaction_bytes = hex::decode(transaction_hex).unwrap();
     let mut bytes_slice = transaction_bytes.as_slice();
     let version = read_version(&mut bytes_slice);
+    let input_length = read_compact_size(&mut bytes_slice);
     println!("bytes slice first element: {:?}", bytes_slice[0]);
     println!("version: {}", version);
+    println!("Input Length: {}", input_length);
 }
