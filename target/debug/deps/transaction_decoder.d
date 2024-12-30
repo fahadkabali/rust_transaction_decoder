840127ce7e1692114496be29793741491c23568f
fahadkabali <kabalifahad@gmail.com>
2024-12-30 03:59:27 +0300

    removed target files

--- target/debug/deps/transaction_decoder.d
+++
@@ -1,5 +0,0 @@
-D:\Projects\Projects\transaction_decoder\target\debug\deps\transaction_decoder.exe: src/main.rs
-
-D:\Projects\Projects\transaction_decoder\target\debug\deps\transaction_decoder.d: src/main.rs
-
-src/main.rs:
--- target/debug/transaction_decoder.d
+++
@@ -1,1 +0,0 @@
-D:\Projects\Projects\transaction_decoder\target\debug\transaction_decoder.exe: D:\Projects\Projects\transaction_decoder\src\main.rs
