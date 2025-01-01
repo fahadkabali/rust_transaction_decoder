0f9d7a07aaf0f0d39738db6f659260caa965a18d
fahadkabali <kabalifahad@gmail.com>
2025-01-02 01:56:27 +0300

    updated the error handling

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::io::{Error as ioError, Read};
+use std::error::Error;
 use transaction::{Amount, Inputs, Outputs, Transaction, Txid};
 use sha2::{Sha256, Digest};
 mod transaction;
@@ -81,9 +82,8 @@ fn hash_raw_transaction(raw_transaction:  &[u8]) -> Txid {
     let result = hasher.finalize();
     Txid::from_bytes(result.into())
 }
-fn main() -> Result<(), ioError> {
-    let transaction_hex = "010000000242d5c1d6f7308bbe95c0f6e1301dd73a8da77d2155b0773bc297ac47f9cd7380010000006a4730440220771361aae55e84496b9e7b06e0a53dd122a1425f85840af7a52b20fa329816070220221dd92132e82ef9c133cb1a106b64893892a11acf2cfa1adb7698dcdc02f01b0121030077be25dc482e7f4abad60115416881fe4ef98af33c924cd8b20ca4e57e8bd5feffffff75c87cc5f3150eefc1c04c0246e7e0b370e64b17d6226c44b333a6f4ca14b49c000000006b483045022100e0d85fece671d367c8d442a96230954cdda4b9cf95e9edc763616d05d93e944302202330d520408d909575c5f6976cc405b3042673b601f4f2140b2e4d447e671c47012103c43afccd37aae7107f5a43f5b7b223d034e7583b77c8cd1084d86895a7341abffeffffff02ebb10f00000000001976a9144ef88a0b04e3ad6d1888da4be260d6735e0d308488ac508c1e000000000017a91476c0c8f2fc403c5edaea365f6a284317b9cdf7258700000000";
-    let transaction_bytes = hex::decode(transaction_hex)?;
+fn decode(transaction_hex: String) -> Result<String, Box<dyn Error>> {
+    let transaction_bytes = hex::decode(transaction_hex).map_err(|e| format!("Hex decode error : {} ", e))?;
     let mut bytes_slice = transaction_bytes.as_slice();
     let version = read_u32(&mut bytes_slice)?;
     let input_count = read_compact_size(&mut bytes_slice)?;
@@ -120,36 +120,45 @@ fn main() -> Result<(), ioError> {
         lock_time,
         
     };
-    let transaction_json = serde_json::to_string_pretty(&transaction).unwrap();
-    println!("Transaction: {}", transaction_json);
-    Ok(())
+    // let transaction_json = serde_json::to_string_pretty(&transaction)?;
+    Ok(serde_json::to_string_pretty(&transaction)?)
+}
+fn main() {
+    let transaction_hex = "0f10000000242d5c1d6f7308bbe95c0f6e1301dd73a8da77d2155b0773bc297ac47f9cd7380010000006a4730440220771361aae55e84496b9e7b06e0a53dd122a1425f85840af7a52b20fa329816070220221dd92132e82ef9c133cb1a106b64893892a11acf2cfa1adb7698dcdc02f01b0121030077be25dc482e7f4abad60115416881fe4ef98af33c924cd8b20ca4e57e8bd5feffffff75c87cc5f3150eefc1c04c0246e7e0b370e64b17d6226c44b333a6f4ca14b49c000000006b483045022100e0d85fece671d367c8d442a96230954cdda4b9cf95e9edc763616d05d93e944302202330d520408d909575c5f6976cc405b3042673b601f4f2140b2e4d447e671c47012103c43afccd37aae7107f5a43f5b7b223d034e7583b77c8cd1084d86895a7341abffeffffff02ebb10f00000000001976a9144ef88a0b04e3ad6d1888da4be260d6735e0d308488ac508c1e000000000017a91476c0c8f2fc403c5edaea365f6a284317b9cdf7258700000000";
+    match decode(transaction_hex.to_string()) {
+        Ok(transaction_json) => println!("Transaction: {}", transaction_json),
+        Err(e) => eprintln!("Error: {}", e),
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::read_compact_size;
+    use super::error::Error;
     #[test]
-    fn test_read_compact_size() {
+    fn test_read_compact_size() -> Result<(), Box<dyn Error>> {
         let mut bytes = [1_u8].as_slice();
-        let count = read_compact_size(&mut bytes);
+        let count = read_compact_size(&mut bytes)?;
         assert_eq!(count, 1_u64);
 
         let mut bytes = [253_u8, 0, 1].as_slice();
-        let count = read_compact_size(&mut bytes);
+        let count = read_compact_size(&mut bytes)?;
         assert_eq!(count, 256_u64);
 
         let mut bytes = [254_u8, 0, 0, 0, 1].as_slice();
-        let count = read_compact_size(&mut bytes);
+        let count = read_compact_size(&mut bytes)?;
         assert_eq!(count, 256_u64.pow(3));
 
         let mut bytes = [255_u8, 0, 0, 0, 0, 0, 0, 0, 1].as_slice();
-        let count = read_compact_size(&mut bytes);
+        let count = read_compact_size(&mut bytes)?;
         assert_eq!(count, 256_u64.pow(7));
 
         let hex = "fd204e";
-        let decoded = hex::decode(hex).unwrap();
+        let decoded = hex::decode(hex)?;
         let mut bytes = decoded.as_slice();
-        let count = read_compact_size(&mut bytes);
+        let count = read_compact_size(&mut bytes)?;
         assert_eq!(count, 20_000_u64);
+
+        Ok(())
     }
 }
